74c4a7a158acbcbc27a8c395d39f8d62c3387e8f
m2lovric <m2lovric@gmail.com>
2022-01-22 23:50:25 +0100

    Simple pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,17 +1,24 @@
 import { useQuery } from 'react-query';
+import { useState } from 'react/cjs/react.development';
 import Planet from './Planet';
 
-const fetchPlanets = async () => {
-  const res = await fetch('https://swapi.dev/api/planets');
+const fetchPlanets = async ({ queryKey }) => {
+  const [_, page] = queryKey;
+  const res = await fetch(`https://swapi.dev/api/planets/?page=${page}`);
   return res.json();
 };
 
 const Planets = () => {
-  const { data, status } = useQuery('planets', fetchPlanets);
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(['planets', page], fetchPlanets);
   console.log(data);
   return (
     <section>
       <h1>Planets</h1>
+      <button onClick={() => setPage(1)}>Page 1</button>
+      <button onClick={() => setPage(2)}>Page 2</button>
+      <button onClick={() => setPage(3)}>Page 3</button>
+
       {status === 'error' && <article>Error fetching data</article>}
       {status === 'loading' && <article>Loading data...</article>}
       {status === 'success' && (
